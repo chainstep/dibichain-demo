86a2f858753208b72c77b555729a9db66917a398
atz3n <attila.aldemir@atz3n.dev>
2022-05-24 09:31:29 +0200

    fix http origin tests

--- company-client/test/http/httpOrigin.test.ts
+++ company-client/test/http/httpOrigin.test.ts
@@ -2,11 +2,22 @@ import request from "supertest";
 import { ROUTE_NAMES } from "../../src/http/constants";
 import { httpServer } from "../../src/http/http";
 import { EnvVars } from "../../src/lib/EnvVars";
+import { ProductStore } from "../../src/storage/product/ProductStore";
+import { ProductStoreInMemory } from "../../src/storage/product/ProductStoreInMemory";
 import { config } from "../config";
+import { TEST_PRODUCT } from "../constants";
 
 
 if (!config.skipTests.includes("httpOrigin")) {
+    const productStore = ProductStore.get();
+
+    beforeEach(async () => {
+        (<ProductStoreInMemory> productStore).clear();
+    });
+
+
     it("should accept requests from known origins", async () => {
+        await productStore.add(TEST_PRODUCT);
         await request(httpServer)
             .get(ROUTE_NAMES.products)
             .set("Origin", EnvVars.ALLOWED_ORIGINS[0])
