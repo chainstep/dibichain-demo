dd34d2a829cc54533db45072a4ea9a4100c79bea
atz3n <attila.aldemir@atz3n.dev>
2022-06-13 16:01:04 +0200

    improve get products route

--- company-client/src/http/routes/products/get/getProducts.ts
+++ company-client/src/http/routes/products/get/getProducts.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from "express";
 import { query } from "express-validator";
 import { ProductStore } from "../../../../storage/product/ProductStore";
 import { INVALID_INPUT_TEXT, ROUTE_NAMES } from "../../../constants";
@@ -12,7 +13,18 @@ export const getProductsRouter = createRouter({
     inputChecks: [
         query("uid").optional().isUUID().withMessage(INVALID_INPUT_TEXT + "uid")
     ],
+    middlewares: [ cleanseParams ],
     service: new GetProductsService({
         getProductStore: () => ProductStore.get()
     })
 });
+
+function cleanseParams(request: Request, response: Response, next: NextFunction): void {
+    const newQuery = <{uid?: string}> {};
+    if (request.query.uid) {
+        newQuery.uid = <string> request.query.uid;
+    }
+
+    request.query = newQuery;
+    next();
+}
