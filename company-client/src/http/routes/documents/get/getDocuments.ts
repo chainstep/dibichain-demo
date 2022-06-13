a6745c4362188bb47b81f95a4e1456f570310e21
atz3n <attila.aldemir@atz3n.dev>
2022-06-13 16:02:31 +0200

    improve get documents route

--- company-client/src/http/routes/documents/get/getDocuments.ts
+++ company-client/src/http/routes/documents/get/getDocuments.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from "express";
 import { query } from "express-validator";
 import { DocumentStore } from "../../../../storage/document/DocumentStore";
 import { INVALID_INPUT_TEXT, ROUTE_NAMES } from "../../../constants";
@@ -12,7 +13,18 @@ export const getDocumentsRouter = createRouter({
     inputChecks: [
         query("uid").optional().isUUID().withMessage(INVALID_INPUT_TEXT + "uid")
     ],
+    middlewares: [ cleanseParams ],
     service: new GetDocumentsService({
         getDocumentStore: () => DocumentStore.get()
     })
 });
+
+function cleanseParams(request: Request, response: Response, next: NextFunction): void {
+    const newQuery = <{uid?: string}> {};
+    if (request.query.uid) {
+        newQuery.uid = <string> request.query.uid;
+    }
+
+    request.query = newQuery;
+    next();
+}
