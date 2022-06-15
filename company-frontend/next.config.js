92336a45d37b87e865d8143650b96913f4cbb628
atz3n <attila.aldemir@atz3n.dev>
2022-06-15 15:20:08 +0200

    fix misconfiguration

--- company-frontend/next.config.js
+++ company-frontend/next.config.js
@@ -14,7 +14,7 @@ module.exports = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     publicRuntimeConfig: {
         BASE_PATH: process.env.BASE_PATH,
-        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL="http://localhost:3100",
-        COMPANY_NAME: process.env.COMPANY_NAME="Logistex (A)"
+        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL,
+        COMPANY_NAME: process.env.COMPANY_NAME
       }
 };
