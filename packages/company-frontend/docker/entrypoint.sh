271ad3a0255f03224744aaa5df1ae19a0f88b3d5
atz3n <attila.aldemir@atz3n.dev>
2022-06-21 08:43:00 +0200

    improve comments

--- packages/company-frontend/docker/entrypoint.sh
+++ packages/company-frontend/docker/entrypoint.sh
@@ -5,6 +5,7 @@ test -n "$BACKEND_BASE_URL"
 echo "Check that we have COMPANY_NAME env"
 test -n "$COMPANY_NAME"
 
+echo "Replace dummy values with values of environment variables"
 find /usr/app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_BACKEND_BASE_URL#$BACKEND_BASE_URL#g"
 find /usr/app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_COMPANY_NAME#$COMPANY_NAME#g"
 
