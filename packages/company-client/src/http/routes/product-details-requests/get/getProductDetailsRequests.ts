2d0888bb65d1777a82e61d2972d56102b2bc92a1
atz3n <attila.aldemir@atz3n.dev>
2022-07-11 10:00:02 +0200

    add product details request route documentation

--- packages/company-client/src/http/routes/product-details-requests/get/getProductDetailsRequests.ts
+++ packages/company-client/src/http/routes/product-details-requests/get/getProductDetailsRequests.ts
@@ -7,6 +7,36 @@ import { createRouter } from "../../../routerFactory";
 import { GetProductDetailsRequestsService } from "./GetProductDetailsRequestsService";
 
 
+/**
+ * @swagger
+ * /product-details-requests:
+ *   get:
+ *     summary: Get received product details requests
+ *     description: This route lets you retrieve the received product details requests
+ *     tags: [Product Details Requests]
+ *     parameters:
+ *       - in: query
+ *         name: uid
+ *         description: The uid of the product. Returns all product details requests if no uid is provided
+ *         schema:
+ *           type: string
+ *         example: 0e66f0e4-1c4e-4781-8fd7-36058206d295
+ *     responses:
+ *       200:
+ *         description: the list of product details requests
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     myNewProducts:
+ *                       type: array
+ *                       items:
+ *                         $ref: '#/components/schemas/ProductDetailsRequest'
+ */
 export function createGetProductDetailsRequestsRouter(): Router {
     return createRouter({
         method: "get",
