10c9497ffbaa7e170c65a7dc9d6cbb72e05142e6
atz3n <attila.aldemir@atz3n.dev>
2022-07-11 09:56:22 +0200

    add product details request route documentation

--- README.md
+++ README.md
@@ -57,17 +57,20 @@ To access the deployed setup follow these links:
 
 - frontend url: https://www.logistex.dibichain.de
 - client url: https://api.logistex.dibichain.de
+- client api docs url: https://api.logistex.dibichain.de/api-docs
 
 
 #### Logistly
 
 - frontend url: https://www.logistly.dibichain.de
 - client url: https://api.logistly.dibichain.de
+- client api docs url: https://api.logistly.dibichain.de/api-docs
 
 
 #### Operator
 
 - api url: https://api.operator.dibichain.de
+- api docs url: https://api.operator.dibichain.de/api-docs
 
 
 #### Block explorer
--- packages/company-client/src/http/routes/my-product-details-requests/get/getMyProductDetailsRequests.ts
+++ packages/company-client/src/http/routes/my-product-details-requests/get/getMyProductDetailsRequests.ts
@@ -7,6 +7,36 @@ import { createRouter } from "../../../routerFactory";
 import { GetMyProductDetailsRequestsService } from "./GetMyProductDetailsRequestsService";
 
 
+/**
+ * @swagger
+ * /my-product-details-requests:
+ *   get:
+ *     summary: Get my product details requests
+ *     description: This route lets you retrieve your product details requests
+ *     tags: [Product Details Requests]
+ *     parameters:
+ *       - in: query
+ *         name: uid
+ *         description: The uid of the product. Returns all product details requests if no uid is provided
+ *         schema:
+ *           type: string
+ *         example: 0e66f0e4-1c4e-4781-8fd7-36058206d295
+ *     responses:
+ *       200:
+ *         description: the list of product details requests
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     myNewProducts:
+ *                       type: array
+ *                       items:
+ *                         $ref: '#/components/schemas/ProductDetailsRequest'
+ */
 export function createGetMyProductDetailsRequestsRouter(): Router {
     return createRouter({
         method: "get",
