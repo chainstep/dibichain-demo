da1d900612e86ea2e0b07fba594be827f7c45b4d
atz3n <attila.aldemir@atz3n.dev>
2022-07-20 09:45:15 +0200

    add bionic partition product

--- resources/docker/entrypoint.sh
+++ resources/docker/entrypoint.sh
@@ -5,10 +5,18 @@ if [ -z ${LOGISTEX_URL} ]; then
     exit 1
 fi
 
+if [ -z ${LOGISTLY_URL} ]; then
+    echo "Error: LOGISTLY_URL is missing"
+    exit 1
+fi
 
-echo "Add Scalmalloy Part..."
 cd /usr/app
+
+echo "Adding Scalmalloy Part to logistex..."
 node ./productAdder.js ${LOGISTEX_URL} ./products/scalmalloy-part/scalmalloy-part.json
 node ./documentAdder.js ${LOGISTEX_URL} 8181c8ae-eef1-4703-8498-2cf25be2877b ./products/scalmalloy-part/Carbon_Footprint_Report_Scalmalloy_AP12345.pdf
+
+echo "Adding Bionic Partition to logistly..."
+node ./productAdder.js ${LOGISTLY_URL} ./products/bionic-partition/bionic-partition.json
 
 exec "$@"
