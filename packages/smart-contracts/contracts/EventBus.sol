1bcd130b4ffdf1f9fcd68c13392ca4967ab604c0
atz3n <attila.aldemir@atz3n.dev>
2022-08-09 09:03:14 +0200

    add contract documentation

--- packages/smart-contracts/contracts/EventBus.sol
+++ packages/smart-contracts/contracts/EventBus.sol
@@ -2,12 +2,29 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "hardhat/console.sol";
 
 
-contract EventBus is Ownable  {
+/**
+ * @title  Dibichain Event Bus Contract
+ * @author CHAINSTEP GmbH
+ * @notice The event bus contract acts as an event bus to distribute events to the Dibichain system.
+ * @dev    In it's current state everyone is able to listen to events, but only the operator is allowed to issue events.
+ */
+contract EventBus is Ownable {
+    /**
+     * @notice Emitted when a new product should be announced
+     * @param  uid - the unique id of the product
+     * @param  productData - the public product data
+     */
     event NewProduct(string indexed uid, ProductData productData);
+
+    /**
+     * @notice Emitted when a product details request should be announced
+     * @param  uid - the unique id of the product
+     * @param  productDetailsRequestData - the product details request data
+     */
     event ProductDetailsRequest(string indexed uid, ProductDetailsRequestData productDetailsRequestData);
+
 
     struct ProductData {
         string uid;
@@ -25,11 +42,19 @@ contract EventBus is Ownable  {
     }
     
 
+    /**
+     * @notice Broadcasts new product events
+     * @param  productData - the public product data
+     */
     function broadcastNewProduct(ProductData memory productData) public onlyOwner {
         emit NewProduct(productData.uid, productData);
     }
 
 
+    /**
+     * @notice Broadcasts product details request events
+     * @param  productDetailsRequestData - the product details request data
+     */
     function broadcastProductDetailsRequest(ProductDetailsRequestData memory productDetailsRequestData) public onlyOwner {
         emit ProductDetailsRequest(productDetailsRequestData.uid, productDetailsRequestData);
     }
