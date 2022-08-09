3571cd225b0fc637bf6cf233bbd6f72abf6d512f
atz3n <attila.aldemir@atz3n.dev>
2022-08-09 16:46:46 +0200

    update index page description

--- packages/company-frontend/src/pages/index.tsx
+++ packages/company-frontend/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Link, Text } from '@chakra-ui/react';
 import Layout from '../components/commons/Layout';
 import Page from '../components/commons/Page';
 
+import Footer from '../components/commons/Footer';
 import Header from '../components/commons/Header';
 import { COMPANY_NAME } from '../constants';
-import Footer from '../components/commons/Footer';
 
 const IndexPage: React.FC = () => {
   return (
@@ -13,9 +13,25 @@ const IndexPage: React.FC = () => {
         <div style={{ flex: '1 0 auto' }}>
           <Header />
 
-          <Heading p={10} textAlign='center'>
-            HELLO {COMPANY_NAME}
+          <Heading pt={10} textAlign='center'>
+            Welcome to the Dibichain Demo
           </Heading>
+          <Heading pt={3} size='md' textAlign='center'>
+            You are logged in to company  { COMPANY_NAME }
+          </Heading>
+
+          <Text align={'center'} pt={10}>
+            New here?
+            Have a look into the project{' '}
+            <Link
+              href='https://github.com/chainstep/dibichain-demo/blob/main/README.md'
+              isExternal
+              color="teal.500"
+            >
+              documentation
+            </Link>
+            .
+          </Text>
         </div>
 
         <div style={{ flexShrink: '0' }}>
