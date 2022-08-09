b2e461521db7a41f09ac526cee5fb3228e139691
atz3n <attila.aldemir@atz3n.dev>
2022-08-09 16:47:17 +0200

    improve footer
    improve footer

--- packages/company-frontend/src/components/commons/Footer.tsx
+++ packages/company-frontend/src/components/commons/Footer.tsx
@@ -1,6 +1,7 @@
 import {
-    Box, Flex, Img, Text, useColorModeValue
+  Box, Flex, Grid, GridItem, Img, Link, Text, useColorModeValue
 } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import React from 'react';
 import { PUBLIC_BASE_PATH } from '../../constants';
 
@@ -27,11 +28,35 @@ const Footer: React.FC = () => {
           <Img
             w='50px'
             src={`${PUBLIC_BASE_PATH}/assets/dibichain_logo.png`}
-          ></Img>
+          />
         </Flex>
-        <Text pt={6} fontSize={'lg'} textAlign={'center'}>
-          DIBICHAIN DEMONSTRATOR
-        </Text>
+        <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+          <GridItem w='100%' h='10'>
+            <Box width={20} height={20} ml={20} mt={6}>
+              <NextLink href='https://github.com/chainstep/dibichain-demo' passHref>
+                <Link>
+                  <Img
+                    w='25px'
+                    src={`${PUBLIC_BASE_PATH}/assets/github.png`}
+                  />
+                </Link>
+              </NextLink>
+            </Box>
+          </GridItem>
+          <GridItem w='100%' h='10'>
+            <Text pt={6} fontSize={'lg'} textAlign={'center'}>
+              Dibichain Demonstrator
+            </Text>
+          </GridItem>
+          <GridItem w='100%' h='10'>
+            <Box display="flex" justifyContent="flex-end" minWidth={150} pr={6}>
+              <Img
+                w='150px'
+                src={`${PUBLIC_BASE_PATH}/assets/BMBF.jpg`}
+              />
+            </Box>
+          </GridItem>
+        </Grid>
       </Box>
   );
 };
