a082bcd104f0e1f3428ee2bdc3431cf67f717dea
atz3n <attila.aldemir@atz3n.dev>
2022-08-22 11:47:10 +0200

    refactor mongo db connection handling

--- packages/company-client/src/storage/AMongoDBStore.ts
+++ packages/company-client/src/storage/AMongoDBStore.ts
@@ -1,8 +1,7 @@
-import { connect, connection, Model, ConnectionStates } from "mongoose";
+import { connect, Model } from "mongoose";
 
 
 export abstract class AMongoDBStore {
-    private readonly CONNECTION_TIMEOUT_SEC = 60;
     private readonly REMOVE_MONGO_FIELDS = "-_id -__v";
     protected model: Model<unknown>;
     protected url: string;
@@ -12,55 +11,23 @@ export abstract class AMongoDBStore {
     constructor(options: {model: Model<any>, url: string}) {
         this.model = options.model;
         this.url = options.url;
-        this.connectToDB();
-    }
-
-    private connectToDB(): void {
-        if (connection.readyState === ConnectionStates.disconnected
-         || connection.readyState === ConnectionStates.disconnecting) {
-            connect(this.url);
-        }
     }
 
 
     protected async _upsert(filter: object, update: object): Promise<void> {
-        await this.waitUntilConnected();
+        await connect(this.url);
         await this.model.updateMany(filter, update, { upsert: true });
-    }
-
-    private async waitUntilConnected(counter = 0): Promise<void> {
-        if (connection.readyState === ConnectionStates.connected) {
-            return;
-        }
-
-        if (connection.readyState === ConnectionStates.disconnected
-         || connection.readyState === ConnectionStates.disconnecting) {
-            await connect(this.url);
-        }
-
-        if (connection.readyState === ConnectionStates.connecting) {
-            if (counter >= this.CONNECTION_TIMEOUT_SEC) {
-                throw new Error("Connection timeout");
-            }
-            counter++;
-            await this.sleep(1000);
-            await this.waitUntilConnected(counter);
-        }
-    }
-
-    private async sleep(timeInMs: number): Promise<void> {
-        await new Promise(r => setTimeout(r, timeInMs));
     }
 
 
     protected async _find<T>(filter: object): Promise<T[]> {
-        await this.waitUntilConnected();
+        await connect(this.url);
         return await this.model.find(filter, this.REMOVE_MONGO_FIELDS).lean();
     }
 
 
     protected async _delete(filter: object): Promise<void> {
-        await this.waitUntilConnected();
+        await connect(this.url);
         await this.model.deleteMany(filter);
     }
 }
