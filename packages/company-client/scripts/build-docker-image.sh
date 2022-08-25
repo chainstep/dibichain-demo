6be9f87d2b6c7f62be8de36847275647694cc689
Kevin Kuhfeldt <kevin.kuhfeldt@gmx.de>
2022-08-25 14:14:13 +0200

    update docker build scripts

--- packages/company-client/scripts/build-docker-image.sh
+++ packages/company-client/scripts/build-docker-image.sh
@@ -20,4 +20,14 @@ HERE="$(pwd)/$(dirname $0)"
 # MAIN
 ###################################################################################################
 
-docker build -f ${HERE}/../docker/Dockerfile ${HERE}/.. -t ${IMAGE_NAME}
+BUILD_CMD="build"
+
+while getopts "a" opt; do
+    case "$opt" in
+    a)  # multi-arch build -> https://blog.jaimyn.dev/how-to-build-multi-architecture-docker-images-on-an-m1-mac/
+        BUILD_CMD="buildx build --platform linux/amd64 --load"
+    esac
+done
+
+echo ${BUILD_CMD}
+docker ${BUILD_CMD} -f ${HERE}/../docker/Dockerfile ${HERE}/.. -t ${IMAGE_NAME}
